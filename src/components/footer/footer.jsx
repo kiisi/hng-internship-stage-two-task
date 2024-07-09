a53c93c82fa39b830e4a41e0c92260b74084aa49
kiisi <kiisifelix06@gmail.com>
2024-07-09 21:52:10 +0100

    fix/ footer links font size

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -13,20 +13,20 @@ export default function Footer() {
                 <div className="mb-10 w-full max-w-[810px] mx-auto flex justify-around flex-wrap items-start gap-[50px] lg:gap-[100px] whitespace-nowrap">
                     <div className="grid place-items-center gap-2">
                         <h2 className="font-medium text-[16px] lg:text-[18px] mb-2">Home</h2>
-                        <div><Link to='/'>Category</Link></div>
-                        <div><Link to='/'>Featured Collection</Link></div>
-                        <div><Link to='/'>Latest Arrival</Link></div>
+                        <div className="text-[14px]"><Link to='/'>Category</Link></div>
+                        <div className="text-[14px]"><Link to='/'>Featured Collection</Link></div>
+                        <div className="text-[14px]"><Link to='/'>Latest Arrival</Link></div>
                     </div>
                     <div className="grid place-items-center gap-2">
                         <h2 className="font-medium text-[16px] lg:text-[18px] mb-2">Support</h2>
-                        <div><Link to='/'>Order Status</Link></div>
-                        <div><Link to='/'>Payment</Link></div>
-                        <div><Link to='/'>Track Order</Link></div>
+                        <div className="text-[14px]"><Link to='/'>Order Status</Link></div>
+                        <div className="text-[14px]"><Link to='/'>Payment</Link></div>
+                        <div className="text-[14px]"><Link to='/'>Track Order</Link></div>
                     </div>
                     <div className="grid place-items-center gap-2">
                         <h2 className="font-medium text-[16px] lg:text-[18px] mb-2">Contact Us</h2>
-                        <div><Link to='/'>lovy@gmail.com</Link></div>
-                        <div><Link to='/'>+12340945688493</Link></div>
+                        <div className="text-[14px]"><Link to='/'>lovy@gmail.com</Link></div>
+                        <div className="text-[14px]"><Link to='/'>+12340945688493</Link></div>
                     </div>
                     <div className="grid place-items-center gap-2">
                         <h2 className="font-medium text-[16px] lg:text-[18px] mb-2">Follow Us</h2>
