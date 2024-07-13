2ece005907b631cc0ae7b94b8652cec04537fa22
kiisi <kiisifelix06@gmail.com>
2024-07-13 23:55:38 +0100

    feat/product back navigation button

--- src/components/home/product.jsx
+++ src/components/home/product.jsx
@@ -2,11 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import productService from "../../services/product";
 import Navbar from "../navbar/navbar";
 import Seo from "../seo";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function Product() {
 
     const { id } = useParams()
+
+    const navigate = useNavigate()
 
     const { data, isLoading, isError } = useQuery({
         queryKey: [`product-${id}`],
@@ -14,7 +16,6 @@ export default function Product() {
     })
 
     const productData = data?.data
-    console.log(productData)
 
     return (
         <>
@@ -31,7 +32,12 @@ export default function Product() {
                                 <div className="absolute shimmer-loading-effect inset-0 h-full w-full"></div>
                             </div>
                         ) : (
-                            <figure>
+                            <figure className="relative">
+                                <button onClick={() => navigate(-1)} className="h-[32px] w-[32px] absolute lg:left-[52px] sm:left-[32px] left-[16px] top-[20px] rounded-full bg-white grid place-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 33 28" fill="none">
+                                        <path d="M33 12.2483H6.86022L16.3459 2.97684L13.8118 0.5L0 14L13.8118 27.5L16.3459 25.0232L6.86022 15.7517H33V12.2483Z" fill="black" />
+                                    </svg>
+                                </button>
                                 <img
                                     src={`https://api.timbu.cloud/images/${productData.photos[0]?.url}`}
                                     alt={productData.name}
